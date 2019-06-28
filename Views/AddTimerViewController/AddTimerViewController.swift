b0778044c5b0c87b06135c2b2e119f87effb1737
Jason Rueckert <phlacin@gmail.com>
2019-06-28 05:41:14 -0700

    Added vc to begin handling ADD timer

---
+++ Views/AddTimerViewController/AddTimerViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddTimerViewController.swift
+//  RealmTimers
+//
+//  Created by Jason on 6/28/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import UIKit
+
+class AddTimerViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timerTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
