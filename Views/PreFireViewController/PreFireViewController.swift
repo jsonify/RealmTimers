f8cf15bd4cfda157f3bdec7421700c3c616c52fb
Jason Rueckert <phlacin@gmail.com>
2019-07-11 23:08:14 -0700

    adding a progress circle as a library

--- Views/PreFireViewController/PreFireViewController.swift
+++ Views/PreFireViewController/PreFireViewController.swift
@@ -9,51 +9,18 @@
 import UIKit
 
 class PreFireViewController: UIViewController {
-let shapeLayer = CAShapeLayer()
+    
+//    circle progress indicator
+//   https://www.youtube.com/watch?v=aylHkGVg-P4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let center = view.center
-        
-        // Create Track layer
-        let trackLayer = CAShapeLayer()
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2 * CGFloat.pi, clockwise: true)
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 10
-        trackLayer.fillColor = UIColor.clear.cgColor
-        view.layer.addSublayer(trackLayer)
-        
-        // Progress layer
-        //        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2 * CGFloat.pi, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
-        
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineWidth = 10
-        shapeLayer.lineCap = .round
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        
-        shapeLayer.strokeEnd = 0
-        
-        view.layer.addSublayer(shapeLayer)
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        // Do any additional setup after loading the view.
+       
     }
 
     @IBAction func closeTapped(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
-    @objc
-    private func handleTap() {
-        print("Now attempting to add stroke")
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.toValue = 1
-        basicAnimation.duration = 2
-        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
-        basicAnimation.isRemovedOnCompletion = false
-        
-        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
-    }
 }
 
