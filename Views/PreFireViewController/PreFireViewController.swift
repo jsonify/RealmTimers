c584003820ddacc13308191194c9655f994aa169
Jason Rueckert <phlacin@gmail.com>
2019-07-12 22:56:53 -0700

    Working with progress circle

--- Views/PreFireViewController/PreFireViewController.swift
+++ Views/PreFireViewController/PreFireViewController.swift
@@ -5,17 +5,30 @@
 //  Created by Jason on 7/8/19.
 //  Copyright © 2019 Jason. All rights reserved.
 //
-
+import MBCircularProgressBar
 import UIKit
 
 class PreFireViewController: UIViewController {
     
 //    circle progress indicator
 //   https://www.youtube.com/watch?v=aylHkGVg-P4
+    @IBOutlet weak var progressView: MBCircularProgressBarView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       self.progressView.value = 0
+        // get the preFire time
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 10.0) {
+            self.progressView.value = 60
+            if self.progressView.value == 30 {
+                self.progressView.progressColor = UIColor.green
+            }
+        }
     }
 
     @IBAction func closeTapped(_ sender: UIButton) {
