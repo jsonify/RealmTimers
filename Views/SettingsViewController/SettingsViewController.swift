acee9781b1e6f2b8e96f352935d56b20644436cb
Jason <phlacin@gmail.com>
2019-07-24 13:23:47 -0700

    Added SettingsViewController

---
+++ Views/SettingsViewController/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  RealmTimers
+//
+//  Created by Jason Rueckert on 7/24/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+
+}
