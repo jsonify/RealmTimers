9d3420ee2160fe554c8368934bcfb50dfee5f7dc
Jason <phlacin@gmail.com>
2019-08-08 15:21:02 -0700

    Adjusting colors

--- Resources/CustomControls/AppUIButton.swift
+++ Resources/CustomControls/AppUIButton.swift
@@ -13,9 +13,9 @@ class AppUIButton: UIButton {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        backgroundColor = #colorLiteral(red: 0.1545447111, green: 0.3621617556, blue: 0.9042051435, alpha: 1)
+        backgroundColor = #colorLiteral(red: 0.231372549, green: 0.3019607843, blue: 0.4078431373, alpha: 1)
         layer.cornerRadius = frame.height / 2
-        setTitleColor(Theme.primaryColor, for: .normal)
+        setTitleColor(#colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1), for: .normal)
     }
 
 }
