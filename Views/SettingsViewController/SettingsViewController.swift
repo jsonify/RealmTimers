0b8f919f87ed794cdee221f4dbe5784d95f143a6
Jason <phlacin@gmail.com>
2019-08-10 00:28:37 -0700

    Added SettingsVC start

--- Views/SettingsViewController/SettingsViewController.swift
+++ Views/SettingsViewController/SettingsViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var closeButton: FloatingActionButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        closeButton.transform = CGAffineTransform(translationX: view.frame.width - 260, y: 0)
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        moveButton()
+    }
+    
+    func moveButton() {
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
+            self.closeButton.transform = .identity
+            
+        }) { (true) in
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut, animations: {
+                self.closeButton.transform = CGAffineTransform(rotationAngle: 45 * -(.pi/180))
+            })
+            
+        }
     }
     
     @IBAction func closeTapped(_ sender: UIButton) {
         dismiss(animated: true)
     }
     
-
+    
 }
