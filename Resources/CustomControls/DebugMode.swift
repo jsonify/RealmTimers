555b1879cef2cfa8193717c42da0a98cd679b523
Jason <phlacin@gmail.com>
2019-08-13 12:13:47 -0700

    For future debug mode

---
+++ Resources/CustomControls/DebugMode.swift
@@ -0,0 +1,13 @@
+//
+//  Debug.swift
+//  RealmTimers
+//
+//  Created by Jason Rueckert on 8/13/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import UIKit
+
+class DebugMode {
+    
+}
