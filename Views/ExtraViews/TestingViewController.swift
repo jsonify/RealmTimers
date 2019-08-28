ca9f5d498a53a653bca9095884a2c8629d8d7295
Jason <phlacin@gmail.com>
2019-08-27 20:27:44 -0700

    Setting up TestViewController to be the preFireVC if selected

--- Views/ExtraViews/TestingViewController.swift
+++ Views/ExtraViews/TestingViewController.swift
@@ -15,9 +15,21 @@ class TestingViewController: UIViewController {
         super.viewDidLoad()
         
         DataTimer.shared.check()
-        
+        showButtons()
         }
+
+    func showButtons() {
+        let closeViewButton: UIButton = UIButton(frame: CGRect(x: view.frame.size.width - 110, y: view.frame.size.height - 100, width: 100, height: 50))
+        closeViewButton.layer.cornerRadius = 10
+        closeViewButton.backgroundColor = .black
+        closeViewButton.setTitle("Close", for: .normal)
+        closeViewButton.addTarget(self, action:#selector(closeView), for: .touchUpInside)
+        self.view.addSubview(closeViewButton)
+    }
     
+    @objc func closeView() {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 
